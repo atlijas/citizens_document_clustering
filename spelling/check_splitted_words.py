5b638c0d7cbc39672b30c12e3cc36ff79f7a7cc5
atlijas <atlijas@simnet.is>
2019-07-17 18:47:17 +0000

    fixed path in spelling

--- spelling/check_splitted_words.py
+++ spelling/check_splitted_words.py
@@ -1,7 +1,7 @@
 from gensim.models import Word2Vec
 
 # W2V model containing inflected word forms
-words_model = Word2Vec.load('/home/atlijas/Desktop/maltol/w2v/malheild_w2v.model')
+words_model = Word2Vec.load('/path_to_w2v_model.model')
 # List of all inflected word forms
 words = words_model.wv.index2entity
 # All words ranked by how often they appear
