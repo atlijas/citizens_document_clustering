e1aefa0c103fe89bba9ba0ca7311104f71483f54
atlijas <atlijas@simnet.is>
2019-07-18 16:59:47 +0000

    fixed a typo

--- spelling/check_splitted_words.py
+++ spelling/check_splitted_words.py
@@ -1,11 +1,11 @@
 from gensim.models import Word2Vec
 
 # W2V model containing inflected word forms
-words_model = Word2Vec.load('/path_to_w2v_model.model')
+words_model = Word2Vec.load('path/to/w2v_model.model')
 # List of all inflected word forms
 words = words_model.wv.index2entity
 # All words ranked by how often they appear
-ranked_words = {k:v for (v,k) in enumerate(words)}
+word_freq = {k:v for (v,k) in enumerate(words)}
 
 
 # Checks whether a word word has been split in two
@@ -14,7 +14,7 @@ def check_splits(text):
     text = text.split()
     for word in range(len(text)):
         try:
-            if text[word]+text[word+1] in ranked_words.keys():
+            if text[word]+text[word+1] in word_freq.keys():
                 print(text[word]+text[word+1])
         except IndexError:
             pass
